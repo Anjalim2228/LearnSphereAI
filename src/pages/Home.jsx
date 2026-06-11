d07a276be6b30874658a96e4e6f08d4408b0e1f7
Anjali <anjaliajitmane22@gmail.com>
2026-06-11 14:59:35 +0530

    initial project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Landing Page</h1>
+    </div>
+  )
+}
+
+export default Home
